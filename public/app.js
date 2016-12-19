5c9fc7215465fcb9380552de74b7711f9dd1aab3
Nicko <nicolasmathieu31@hotmail.com>
2016-12-19 13:44:46 +0100

    Récuperation de la valeur en vanilla

---
+++ public/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// function hello (person = 'Père Noel'){
+// console.log('bonjour' + person);
+// }
+// hello('nico');
+// hello();
+
+validText.addEventListener("click", function (event) {
+	console.log('sa click');
+	var inputValue = document.getElementById('inputText').value;
+	console.log(inputValue);
+});
---
+++ src/app.es6.js
@@ -0,0 +1,13 @@
+// function hello (person = 'Père Noel'){
+// console.log('bonjour' + person);
+// }
+// hello('nico');
+// hello();
+
+validText.addEventListener("click",(event)=>{
+	console.log('sa click');
+	let inputValue = document.getElementById('inputText').value;
+	console.log(inputValue);
+});
+
+
