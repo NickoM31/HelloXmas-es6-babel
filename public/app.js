a5b28a2d2d0db57086009649ba3c8ec2ef3853d1
Nicko <nicolasmathieu31@hotmail.com>
2016-12-21 16:16:12 +0100

    fin exo

--- public/app.js
+++ public/app.js
@@ -2,15 +2,34 @@
 
 var tab = [];
 var validText = document.getElementById('btnText');
-validText.addEventListener("click", function (event) {
+var searchBtn = document.getElementById('btnSearch');
+
+validText.addEventListener('click', function (event) {
 	var inputValue = document.getElementById('inputText').value;
-	var ul = document.getElementById('list');
-	var li = document.createElement("li");
+	// console.log(inputValue);
+	var ul = document.getElementById('listName');
+	var li = document.createElement('li');
 	ul.appendChild(li);
 	li.innerHTML = inputValue;
 	if (inputValue === "") {
-		li.innerHTML = "Père Noël!";
+		li.innerHTML = "Père Noël";
 	}
 	tab.push(inputValue);
 	console.log(tab);
+});
+searchBtn.addEventListener('click', function (event) {
+	var filteredTab = [];
+	var searchValue = document.getElementById('searchText').value;
+	var showFilteredName = document.getElementById('affichName');
+	for (var i = 0; i < tab.length; i++) {
+		var element = tab[i];
+		// console.log(element);
+		var filterValue = element.indexOf(searchValue);
+		// console.log(filterValue);
+		if (filterValue === 0) {
+			filteredTab.push(element);
+			console.log(filteredTab);
+			showFilteredName.innerHTML = filteredTab;
+		}
+	}
 });
--- src/app.es6.js
+++ src/app.es6.js
@@ -1,17 +1,40 @@
-let tab = [];
-let validText = document.getElementById('btnText');
-validText.addEventListener("click",(event)=>{
+const tab = [];
+const validText = document.getElementById('btnText');
+const searchBtn = document.getElementById('btnSearch');
+
+validText.addEventListener('click',(event)=>{
 	let inputValue = document.getElementById('inputText').value;
-	let ul = document.getElementById('list');
-	let li = document.createElement("li");
+	// console.log(inputValue);
+	let ul = document.getElementById('listName');
+	let li = document.createElement('li');
 	ul.appendChild(li);
 	li.innerHTML = inputValue;
 	if(inputValue === ""){
-		li.innerHTML = "Père Noël!";
+		li.innerHTML = "Père Noël";
 	}
 	tab.push(inputValue);
 	console.log(tab);
 });
+searchBtn.addEventListener('click', (event)=>{
+	let filteredTab = [];
+	let searchValue = document.getElementById('searchText').value;
+	let showFilteredName = document.getElementById('affichName');
+	for(let i = 0; i < tab.length; i++){
+		let element = tab[i];
+		// console.log(element);
+		let filterValue = element.indexOf(searchValue);
+		// console.log(filterValue);
+		if(filterValue === 0){
+			filteredTab.push(element);
+			console.log(filteredTab);
+			showFilteredName.innerHTML = filteredTab;
+		}
+	}
+});
+
+
+
+
 
 
 
